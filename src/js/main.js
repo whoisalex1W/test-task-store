808da3f848515eb3c107534cdbf6070f825e6807
whoisalex1W <olexander.vyganiailo@gmail.com>
2023-03-07 00:19:47 +0200

    Add technique on map

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-function manHover(element) {
-    element.setAttribute("src", "/img/man_filled.png");
-    console.log("hover");
-}
-
-function manUnhover(element) {
-    element.setAttribute("src", "/img/man_empty.png");
-}
